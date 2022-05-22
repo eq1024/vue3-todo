f6ef6341c39d0d49285296e6201a6cdd76092b97
二柒 <EQ1024@foxmail.com>
2022-05-22 13:35:20 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <todo></todo>
+</template>
+
+<script>
+import todo from './components/todo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    todo
+  }
+}
+</script>
---
+++ src/components/todo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- new todo -->
+    <input type="text" v-model="newtodo" @keyup.enter="addtodo" placeholder="ADD TODO" autocomplete="off">
+    <!-- show todo -->
+    <ul v-for="todo in todolist" :key="todo.id">
+      <li>{{todo.title}} --- {{todo.completed}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { computed, reactive, toRefs } from 'vue'
+
+export default {
+  setup() {
+    const state = reactive({
+      newtodo: '',
+      todolist: []
+    })
+    function addtodo() {
+      state.todolist.push({
+        id: state.todolist.length + 1,
+        title: state.newtodo,
+        completed: false
+      })
+      state.newtodo = ''
+    }
+    return {
+      ...toRefs(state),
+      addtodo
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
